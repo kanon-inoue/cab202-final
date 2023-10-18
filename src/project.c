b01ac8a25be9399b36a7a3748c9d72a68c5a91fc
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2023-10-18 13:49:30 +1000

    pb_state doesnt match pin4bm

--- src/project.c
+++ src/project.c
@@ -48,12 +48,20 @@ volatile uint8_t pb_state;
 
 ISR(TCB1_INT_vect) { // for timer
   static uint32_t sequence[200];
-  static uint8_t count = 0;
+
+  // Periodic 5 ms interrupt
+  static uint8_t count0 = 0;
+  static uint8_t count1 = 0;
   uint8_t pb_sample = PORTA.IN; // Sample PB state
   uint8_t pb_changed = pb_sample ^ pb_state; // Detect change to PB
-  if (pb_changed && count >= 20) {
-    pb_state = pb_sample;
-    spi_write(PORTA.IN);
+  // pb_changed = pb_changed & pb_state;
+  // Increment if PB state changed, reset otherwise
+  count1 = (count1 ^ count0) & pb_changed;
+  count0 = ~count0 & pb_changed;
+
+  // Update PB state immediately on falling edge or if PB high for three samples
+  pb_state ^= (count1 & count0) | (pb_changed & pb_sample);
+  if (pb_changed) {
     if (pb_state == PIN4_bm) {
       inputs[current_input] = 1;
       current_input += 1;
@@ -67,9 +75,7 @@ ISR(TCB1_INT_vect) { // for timer
       inputs[current_input] = 4;
       current_input += 1;
     }
-    count = 0;
   }
-  count++;
 
   switch (state) {
     case BLANK_DISPLAY:
@@ -135,7 +141,7 @@ ISR(TCB1_INT_vect) { // for timer
           elapsed_time = 0;
         }
         if (current_input == 0) {
-          spi_write(0xBB); // 8 display
+          spi_write(0x00); // 8 display
         }
         if (current_input == 5) {
           spi_write(0xBB);
@@ -164,35 +170,35 @@ ISR(TCB1_INT_vect) { // for timer
       }
   }
   elapsed_time++;
-  TCB0.INTFLAGS = TCB_CAPT_bm; // Acknowledge interrupt
+  TCB1.INTFLAGS = TCB_CAPT_bm; // Acknowledge interrupt
 }
 
 ISR(PORTA_PORT_vect) { // for button 
   if (state == RECV_IDENT) {
     if (VPORTA.INTFLAGS & PIN4_bm) { // push s1
       VPORTA_INTFLAGS = PIN4_bm;
-      // desplay_play_note(1);
+      desplay_play_note(1);
       buzzer_switch = 1;
       elapsed_time = 0;
       //inputs[current_input] = 1;
       // current_input += 1;
     } else if (VPORTA.INTFLAGS & PIN5_bm) { // push s2
       VPORTA_INTFLAGS = PIN5_bm;
-      // desplay_play_note(2);
+      desplay_play_note(2);
       buzzer_switch = 1;
       elapsed_time = 0;
       //inputs[current_input] = 2;
       // current_input += 1;
     } else if (VPORTA.INTFLAGS & PIN6_bm) { // s3
       VPORTA_INTFLAGS = PIN6_bm;
-      // desplay_play_note(3);
+      desplay_play_note(3);
       buzzer_switch = 1;
       elapsed_time = 0;
       //inputs[current_input] = 3;
       // current_input += 1;
     } else if (VPORTA.INTFLAGS & PIN7_bm) { // s4
       VPORTA_INTFLAGS = PIN7_bm;
-      // desplay_play_note(4);
+      desplay_play_note(4);
       buzzer_switch = 1;
       elapsed_time = 0;
       //inputs[current_input] = 4;
